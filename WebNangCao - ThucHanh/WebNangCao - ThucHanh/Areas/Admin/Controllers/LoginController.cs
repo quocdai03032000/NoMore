4b34c1ddfde8416993ed76d0436b3f94d2b1be39
quocdai03032000 <quocdai03032000@gmail.com>
2021-04-07 08:10:04 +0700

    update chuc nang login Admin

---
+++ WebNangCao - ThucHanh/WebNangCao - ThucHanh/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebNangCao___ThucHanh.Models;
+
+namespace WebNangCao___ThucHanh.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        QuanLyThuVienEntities database = new QuanLyThuVienEntities();
+        // GET: Admin/Login
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult login(Account_Admin ad)
+        {
+            var check = database.Account_Admin.Where(s => s.User.Equals(ad.User) && s.Password.Equals(ad.Password)).FirstOrDefault();
+            if(check==null)
+            {
+                ViewBag.Error = "Sai tên đăng nhập hoặc mật khẩu!";
+                return View("Index",ad);
+            }
+            else
+            {
+                Session["User"] = ad.User;
+                return RedirectToAction("Suss", "Login");
+            }
+            
+        }
+
+        public ActionResult Suss()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebNangCao - ThucHanh/WebNangCao - ThucHanh/Areas/Admin/Controllers/MainController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebNangCao___ThucHanh.Areas.Admin.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Admin/Main
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
